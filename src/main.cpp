3509d4c0d63e95d5e33329553a194ba87cadc29b
prutskov <lehaprutskov@gmail.com>
2018-11-29 02:06:21 +0300

    Added computing on host

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 #include <ctime>
 
-const size_t NROWS1 = 2;
-const size_t NCOLS1 = 10;
-const size_t NROWS2 = 10;
-const size_t NCOLS2 = 5;
+const int NROWS1 = 1000;
+const int NCOLS1 = 1000;
+const int NROWS2 = 1000;
+const int NCOLS2 = 1000;
 
 int matrix1 [NROWS1][NCOLS1];
 int matrix2 [NROWS2][NCOLS2];
-int matrixResult [NROWS1][NCOLS2];
+int matrixResultCPU [NROWS1][NCOLS2];
+int matrixResultGPU [NROWS1][NCOLS2];
 
 void InitializeData()
 {   
     /*Initialize matrix 1*/
-    for (size_t i = 0; i< NROWS1; i++ )
+    for (int i = 0; i< NROWS1; i++ )
     {
-        for (size_t j = 0; j< NCOLS1; j++ )
+        for (int j = 0; j< NCOLS1; j++ )
         { 
             matrix1[i][j] = rand() % 101;
         }  
     }
 
     /*Initialize matrix 2*/
-    for (size_t i = 0; i< NROWS2; i++ )
+    for (int i = 0; i< NROWS2; i++ )
     {
-        for (size_t j = 0; j< NCOLS2; j++ )
+        for (int j = 0; j< NCOLS2; j++ )
         {
             matrix2[i][j] = rand() % 101;
         }  
@@ -33,12 +34,35 @@ void InitializeData()
 
 void ComputingOnHost()
 {
+    std::cout<<"-----------------Host-----------------\n";
+    clock_t startTime, endTime;
+
+    std::cout<<"Computing..............";
+
+    startTime = clock();  
+    for (int i = 0 ; i< NROWS1; i++)
+    {        
+        for (int j = 0 ; j< NCOLS2; j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < NCOLS1; k++)
+            {
+                sum += matrix1[i][k]*matrix2[k][j];
+            }
+            matrixResultCPU[i][j] = sum;
+        }    
+    }
+    endTime = clock();
+
+    std::cout<<"OK\n";
+    double time = (double)(endTime - startTime)/CLOCKS_PER_SEC*1000;
+    std::cout<<"Time: "<<time<<" ms"<<std::endl;
 
 }
 
 int main (int argc, char **argv)
 {
     srand(time(NULL));
-    std::cout<<"Hello world\n";
+    ComputingOnHost();
     return 0;
 }
